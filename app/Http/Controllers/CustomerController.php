211f3b21658af3ee52e97bd3936209de6b226ee3
moshiur rahman <moshiurcse888@gmail.com>
2021-10-20 19:32:05 +0600

    Query Builder test for customer update

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -15,7 +15,12 @@ class CustomerController extends Controller
         // $data = DB::table('customers')->where('name', 'moshiur 2')->first();
         // $data = DB::table('customers')->where('id', 5)->first();
         // $data = DB::table('customers')->find(9);
-        $data = DB::table('customers')->find(9);
+        // $data = DB::table('customers')->pluck('name');
+        // $data = DB::table('customers')->count('name');
+        // $data = DB::table('customers')->count();
+        // $data = DB::table('customers')->select('name', 'email as customer_email')->where('votes', 129)->get();
+        // $data = DB::table('customers')->select('name', 'email as customer_email')->where('votes', 129)->first();
+        $data = DB::table('customers')->select('name', 'email as customer_email')->where('votes', 129)->first();
 
         dd($data);
     }
